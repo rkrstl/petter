4e5b7a964ac5dcf3b5264bbdc6aa46026898a711
rkrstl <choiihwa68@gmail.com>
2020-09-23 20:51:12 +0900

    投稿するtweetのテーブル

---
+++ db/migrate/20200923113546_create_tweets.rb
@@ -0,0 +1,11 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.references :user,null:false,foreign_key: true
+      t.string :title,null:false
+      t.text :text,null:false
+
+      t.timestamps
+    end
+  end
+end
