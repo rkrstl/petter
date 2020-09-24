e40d9935879acab0d3b27867f5f50ca0f7d0bb17
rkrstl <choiihwa68@gmail.com>
2020-09-24 17:59:14 +0900

    ユーザー編集画面に遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,28 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
   end
+
+  def edit
+     user=User.find(params[:id])
+     @image=user.image
+  end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to user_path(current_user)
+
+      else
+        render :edit
+      end
+    end
+
+  
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname,:email,:image,:introduction)
+  end
+  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_121152) do
+ActiveRecord::Schema.define(version: 2020_09_24_073020) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_09_23_121152) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
