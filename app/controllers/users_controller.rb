5573590461c601b25357bc7aa611600c57804283
rkrstl <choiihwa68@gmail.com>
2020-09-30 12:47:07 +0900

    ユーザーの詳細画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:nickname,:email,:image,:introduction)
+    params.require(:user).permit(:nickname,:email,:image,:introduction,:pet_name)
   end
   
 
