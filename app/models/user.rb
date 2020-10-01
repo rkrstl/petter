54d1fde2e699302b9c50711f904ba246f3cb19e3
rkrstl <choiihwa68@gmail.com>
2020-10-01 16:02:25 +0900

    userのvalidates

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
 has_one_attached :image
 has_many :tweets
 has_many :likes
 has_many :comments
+
+
+
+
+VALID_PASSWORD_REGEX =/\A[a-zA-Z0-9]+\z/
+validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, on: :create
+
+
+validates :nickname, presence: true
+
+validates :birth,presence:true
+
+validates :email,presence:true,uniqueness: true,format:{with: /\A\S+@\S+\.\S+\z/}
+
+
+
+
 
 
 
