ad458fa220ee2f1139a21d73300447049af618d7
rkrstl <choiihwa68@gmail.com>
2020-10-08 14:35:13 +0900

    個人の予定を登録して表示

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,35 @@
 class PlansController < ApplicationController
+
+
+  def index
+
+    @plans = current_user.plans
+  end
+
+  def new
+     @plan= Plan.new
+  end
+
+  def create
+    @plan = Plan.create(plan_params)
+    if @plan.save
+      redirect_to root_path
+    else
+      render :new
+   end
+  end
+
+
+  def show
+    @plan=Plan.find(params[:id])
+    
+  end
+
+private
+ 
+def plan_params
+  params.require(:plan).permit(:title,:content,:start_time).merge(user_id:current_user.id)
+end
+
+
 end
